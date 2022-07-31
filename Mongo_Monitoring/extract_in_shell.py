cec0b23e43e8a5655761976fab7a385e169e6dc5
Mac_yj <pknb213@naver.com>
2022-07-31 23:47:15 +0900

    update

--- Mongo_Monitoring/extract_in_shell.py
+++ Mongo_Monitoring/extract_in_shell.py
@@ -1,6 +1,7 @@
 # import
 import subprocess
-
+from pymongo import MongoClient
+from pprint import pprint
 # class
 # res = subprocess.run(["ls", "-l", "./"], capture_output=True)
 # print(res.stdout.decode("utf-8"))
@@ -14,28 +15,47 @@ import subprocess
 
 # Todo: Mongo Cluster에 적재하는 것한 후에 Polaris Dummy Data 넣기, sed 명령어 대신 Python String 변환 하기
 
-with subprocess.Popen(["mongo", "--port", "20000", "--eval", "db.stats(1024)", "--quiet"], stdout=subprocess.PIPE) as cp:
-    sed_1 = subprocess.Popen(["sed", "s/\(NumberLong([[:punct:]]\?\)\([[:digit:]]*\)\([[:punct:]]\?)\)/\2/"], stdin=cp.stdout, stdout=subprocess.PIPE)
-    cp.wait()
-    sed_2 = subprocess.Popen(["sed", "s/\(ISODate(\)\(.*\)\()\)/\2/"], stdin=sed_1.stdout, stdout=subprocess.PIPE)
-    sed_1.wait()
-    sed_3 = subprocess.Popen(["sed", "s/Timestamp(\([0-9]*\),[[:space:]]*[[:digit:]]*)/\1/"], stdin=sed_2.stdout, stdout=subprocess.PIPE)
-    sed_2.wait()
-    sed_4 = subprocess.Popen(["sed", "s/BinData([[:digit:]],\([^,]*\))/\1/"], stdin=sed_3.stdout, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
-    res = sed_4.communicate()[0]
-    sed_4.wait()
-    print(res)
+# client = MongoClient("mongodb://localhost:30001,localhost:30002,localhost:30003/?replicaSet=configSet&w=majority")
+client = MongoClient("mongodb://localhost:50001,localhost:50002,localhost:50003/?replicaSet=configSet")
+db = client.test
+coll = db.testcoll
 
-    with open('db_stats', 'w') as f:
-        f.write(res)
+doc = {
+    "name": "people",
+    "age": 20,
+    "nick": "야미"
+}
 
-with open('test.txt', 'w') as f:
-    mongo = subprocess.Popen(["mongo", "--port", "20000", "--eval", "db.serverStatus({'repl':1})"], stdout=subprocess.PIPE)
-    head = subprocess.Popen(["head", "-n", "5"], stdin=mongo.stdout, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
-    mongo.stdout.close()
-    output = head.communicate()
-    print(output[0])
-    # f.write(output[0])
+print(client, "\n", db, "\n", coll, "\n", doc)
+print(client.list_database_names())
+print(db.list_collection_names())
+print(coll.insert_one(doc))
+for doc in coll.find():
+    print(doc)
+
+
+# with subprocess.Popen(["mongo", "--port", "20000", "--eval", "db.stats(1024)", "--quiet"], stdout=subprocess.PIPE) as cp:
+#     sed_1 = subprocess.Popen(["sed", "s/\(NumberLong([[:punct:]]\?\)\([[:digit:]]*\)\([[:punct:]]\?)\)/\2/"], stdin=cp.stdout, stdout=subprocess.PIPE)
+#     cp.wait()
+#     ®sed_2 = subprocess.Popen(["sed", "s/\(ISODate(\)\(.*\)\()\)/\2/"], stdin=sed_1.stdout, stdout=subprocess.PIPE)
+#     sed_1.wait()
+#     sed_3 = subprocess.Popen(["sed", "s/Timestamp(\([0-9]*\),[[:space:]]*[[:digit:]]*)/\1/"], stdin=sed_2.stdout, stdout=subprocess.PIPE)
+#     sed_2.wait()
+#     sed_4 = subprocess.Popen(["sed", "s/BinData([[:digit:]],\([^,]*\))/\1/"], stdin=sed_3.stdout, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
+#     res = sed_4.communicate()[0]
+#     sed_4.wait()
+#     print(res)
+#
+#     with open('db_stats', 'w') as f:
+#         f.write(res)
+#
+# with open('test.txt', 'w') as f:
+#     mongo = subprocess.Popen(["mongo", "--port", "20000", "--eval", "db.serverStatus({'repl':1})"], stdout=subprocess.PIPE)
+#     head = subprocess.Popen(["head", "-n", "5"], stdin=mongo.stdout, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
+#     mongo.stdout.close()
+#     output = head.communicate()
+#     print(output[0])
+#     # f.write(output[0])
 
 
 
